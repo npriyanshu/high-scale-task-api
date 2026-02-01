eb2b0284663566dd4e06d2b6c4c7d456bee47b7b
Priyanshu Negi <npriyanshu63@gmail.com>
2026-02-01 14:25:51 +0530

    feat: Add Docker Compose configuration for core infrastructure and application services, including RabbitMQ queue setup.

--- app/src/config/rabbitmq.ts
+++ app/src/config/rabbitmq.ts
@@ -5,22 +5,22 @@ let channel: amqp.Channel | null = null;
 export async function getRabbitChannel() {
     if (channel) return channel;
 
-    const connection = await amqp.connect("amqp://guest:guest@localhost:5672");
+    const connection = await amqp.connect(process.env.RABBITMQ_URL || "amqp://guest:guest@localhost:5672");
 
     channel = await connection.createChannel();
 
     // DLQ (Dead Letter Queue)
-    await channel.assertQueue("task.notifications.dlq",{
-        durable:true,
+    await channel.assertQueue("task.notifications.dlq", {
+        durable: true,
     })
 
 
     // Main Queue
-    await channel.assertQueue("task.notifications",{
-        durable:true,
+    await channel.assertQueue("task.notifications", {
+        durable: true,
         arguments: {
-            "x-dead-letter-exchange":"",
-            "x-dead-letter-routing-key":"task.notifications.dlq",
+            "x-dead-letter-exchange": "",
+            "x-dead-letter-routing-key": "task.notifications.dlq",
         }
     })
 
